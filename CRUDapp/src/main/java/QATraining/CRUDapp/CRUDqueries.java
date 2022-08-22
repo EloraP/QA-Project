ffeb989a854b84989141c9623b77e0527afcd72c
EloraP <eloraira@gmail.com>
2022-08-22 09:59:02 +0100

    add connection to the database in CRUDchoices and define all methods

--- CRUDapp/src/main/java/QATraining/CRUDapp/CRUDqueries.java
+++ CRUDapp/src/main/java/QATraining/CRUDapp/CRUDqueries.java
@@ -1,5 +1,46 @@
 package QATraining.CRUDapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CRUDqueries {
+	
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet rs;
+	
+	public CRUDqueries() {
+		try {
+			conn = DriverManager.getConnection(DBConfig.URL, DBConfig.USER, DBConfig.PASS);
+			stmt = conn.createStatement();// create a statement object to execute sql queries
+			System.out.println("Connection Successful!");
+		} catch (SQLException e) {
+			System.out.println("Incorrect credentials");
+			e.printStackTrace();
+		}
+	}
+	
+	public void create(Strike s) {
+		System.out.println("create strike");
+	}
+	
+	public void view() {
+		System.out.println("view all database");
+	}
+	
+	public void update(int id, String value, String feature) {
+		System.out.println("update " + id);
+	}
+	
+	public void delete(int id) {
+		System.out.println("delete " + id);
+	}
+	
+	public void close() {
+		System.out.println("close connection");
+	}
 
 }
--- CRUDapp/src/main/java/QATraining/CRUDapp/Runner.java
+++ CRUDapp/src/main/java/QATraining/CRUDapp/Runner.java
@@ -1,28 +1,13 @@
 package QATraining.CRUDapp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class Runner {
 	
-	private static Connection conn;
-	private static Statement stmt;
-	private static ResultSet rs;
 	
     public static void main( String[] args )
     {
-    	try {
-			conn = DriverManager.getConnection(DBConfig.URL, DBConfig.USER, DBConfig.PASS);
-			stmt = conn.createStatement();// create a statement object to execute sql queries
-			System.out.println("Connection Successful!");
-		} catch (SQLException e) {
-			System.out.println("Incorrect credentials");
-			e.printStackTrace();
-		}
+    	CRUDqueries q = new CRUDqueries();
     	
     	Choices start = new Choices();
         start.options();
